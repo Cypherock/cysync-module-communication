afcf4ba06fffa7eea6adfc66fa988e68b27da527
Tejasv Sharma <tejasvonly@gmail.com>
2023-03-17 17:51:02 +0530

    feat: update token list and logic to use tokens in all evm chains

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -4,18 +4,10 @@ import { Erc20CoinData } from './types/Erc20CoinData';
 
 export const getErc20Tokens = (parent: CoinData) => {
   const TOKENSLIST: Record<string, Erc20CoinData> = {};
-  let tokensList: any[];
-
-  switch (parent.id) {
-    case 'ethereum':
-      tokensList = erc20List;
-      break;
-    default:
-      tokensList = [];
-  }
+  const tokensList: any = erc20List;
 
   for (const token of tokensList) {
-    if (token.symbol.length <= 16) {
+    if (token.symbol.length <= 16 && token.platforms[parent.id]) {
       const id = `${parent.id}:${token.id}`;
       TOKENSLIST[id] = new Erc20CoinData({
         id,
@@ -23,8 +15,8 @@ export const getErc20Tokens = (parent: CoinData) => {
         oldId: token.symbol.toLowerCase(),
         abbr: token.symbol.toLowerCase(),
         coinGeckoId: token.id,
-        address: token.address,
-        decimal: token.decimal ?? 18,
+        address: token.platforms[parent.id].contract_address,
+        decimal: token.platforms[parent.id].decimal_place ?? 18,
         name: token.name,
         validatorCoinName: parent.validatorCoinName,
         validatorNetworkType: parent.validatorNetworkType

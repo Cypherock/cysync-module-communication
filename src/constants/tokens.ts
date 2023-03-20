6d371a1c4280cb8b030f0b2332770332dbe00759
Tejasv Sharma <tejasvonly@gmail.com>
2023-03-20 13:00:45 +0530

    chore: lint

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -1,4 +1,5 @@
 import { logger } from '../utils';
+
 import erc20List from './erc20List.json';
 import { CoinData } from './types/CoinData';
 import { Erc20CoinData } from './types/Erc20CoinData';
@@ -9,12 +10,11 @@ export const getErc20Tokens = (parent: CoinData) => {
 
   for (const token of tokensList) {
     if (token.symbol.length <= 16 && token.platforms[parent.id]) {
-
       if (
         !token.platforms[parent.id].contract_address ||
-        token.platforms[parent.id].decimal_place === undefined 
-      ){
-        logger.error({token,parent});
+        token.platforms[parent.id].decimal_place === undefined
+      ) {
+        logger.error({ token, parent });
         throw new Error('Missing token data');
       }
 
